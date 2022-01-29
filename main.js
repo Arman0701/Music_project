8bf3fb154fb2c662e1a089ebd07fa5850c8707ea
Arman773 <tadevosyan889@gmail.com>
2022-01-29 20:59:09 +0400

    fix: fixed some issues

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const navBarButton = document.querySelector('.header_nav');
 navBarButton.addEventListener("click", ()=>{
     document.querySelector('.menuWrapper').classList.remove('closeNavMenu');
     document.querySelector('.menuWrapper').classList.add('loadingNavMenu');
-    document.querySelector('.fullPage').style.display = 'none';
+    document.querySelector('.all_content').style.display = 'none';
     document.querySelector('.menuWrapper').style.display = 'block';
 })
 
@@ -12,7 +12,7 @@ navMenuButton.addEventListener("click", ()=>{
     document.querySelector('.menuWrapper').classList.add('closeNavMenu');
     setTimeout(()=>{
       document.querySelector('.menuWrapper').style.display = 'none';
-      document.querySelector('.fullPage').style.display = 'block';
+      document.querySelector('.all_content').style.display = 'block';
     }, 1950);
 });
 
